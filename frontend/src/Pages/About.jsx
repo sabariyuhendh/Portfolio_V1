7e7f25f6ae5aef152a4b4516dc2d1e8b83b40c47
sabariyuhendh <yuhendhran@gmail.com>
2025-04-12 15:20:08 +0530

    feat(About): add experience section and improve spacing
    Enhance the About page by adding a new "Experience" section and improving the spacing between elements for better readability.

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function About() {
   return (
     <>
-      <div className="flex flex-col justify-center min-h-screen">
+      <div className="flex flex-col justify-center min-h-screen gap-2">
         <h1 className="text-4xl font-bold mb-4">About Me</h1>
-        <p>
+        <p className="mb-4">
           Hi, I'm Sabari Yuhendhran, Currently, I'm diving deep into web
           development and enhancing my skills to create impactful and
           user-friendly web applications. I'm passionate about building
@@ -13,6 +13,12 @@ function About() {
           creating seamless user experiences. My goal is to continue learning
           and growing as a web developer while working on projects that bring
           real value to users.
+        </p>
+        <h1 className="text-4xl font-bold mb-4">Experiance</h1>
+        <p>
+          I have a solid background in web development, with a focus on frontend
+          technologies. I have experience with HTML, CSS, JavaScript, and
+          popular frontend frameworks like React. I am proficient in creating...
         </p>
       </div>
     </>
